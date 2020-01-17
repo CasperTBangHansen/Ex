d3eebf4f2690ff8de48b273657b09be20b3580b2
s176531 <59647352+s176531@users.noreply.github.com>
2020-01-17 09:40:20 +0100

    Controlsystem

--- Eksamensprojekt/src/ControlSystem.c
+++ Eksamensprojekt/src/ControlSystem.c
@@ -4,9 +4,12 @@
     Input *there: Struct value that updates with every move the player makes
 */
 #include "ControlSystem.h"
+#include "Ships.h"
+#include "Drawing.h"
 
 #define ESC 0x1B
 #define SPACE 0x20
+#define ENT 0x0D
 
 
 // Function from the assignments that lets us jump to whereever on the prompt
@@ -32,58 +35,95 @@ void clreol() {
 
 
 // Main function that moves the player around
-void userInput(struct playerPos *there){ // We want to remove that this input is necessary (Only possible with global variable?)<------------------------------------------
-char buttonPress; // Adds a variable to use for registering the pressed button
-uint16_t xPos = (*there).xPosition; // Updates the value of the x-coordinate
-uint8_t yPos = (*there).yPosition; // Updates the value of the y-coordinate
-buttonPress = uart_get_char(); // Defining the variable as the button the player has pressed
-if (buttonPress == '\0'){ // Necessary to make the function not accept null values. Makes it run faster/smoother
-    return buttonPress;
-}
-if(buttonPress != '\0' && buttonPress == 'w' ){ // If and only if the pressed button is "w", the players moves up
-    if (yPos > 0){ // Prevents the player from going off screen
-        gotoxy(xPos,yPos);
-        printf(" ");
-        yPos--;
-        gotoxy(xPos,yPos);
-        printf("O");
-    }
-}
-if(buttonPress != '\0' && buttonPress == 'a'){ // If and only if the pressed button is "a", the players moves left
-    if (xPos > 0){ // Prevents the player from going off screen
-        gotoxy(xPos,yPos);
-        printf(" ");
-        xPos--;
-        gotoxy(xPos,yPos);
-        printf("O");
-    }
-}
-if(buttonPress != '\0' && buttonPress == 's'){ // If and only if the pressed button is "s", the players moves down
-    if (yPos < 66){ // Prevents the player from going off screen
-        gotoxy(xPos,yPos);
-        printf(" ");
-        yPos++;
-        gotoxy(xPos,yPos);
-        printf("O");
+uint8_t UserInput(){
 
-    }
+char buttonPress = uart_get_char(); // Defining the variable as the button the player has pressed
+
+if (buttonPress == '\0'){ // Necessary to make the function not accept null values. Makes it run faster/smoother
+    return 0;
 }
-if(buttonPress != '\0' && buttonPress == 'd'){ // If and only if the pressed button is "d", the players moves right
-    if (yPos < 271){ // Prevents the player from going off screen
-        gotoxy(xPos,yPos);
-        printf(" ");
-        xPos++;
-        gotoxy(xPos,yPos);
-        printf("O");
-    }
+// 1 - 4 are for the WASD controls
+if(buttonPress != '\0' && buttonPress == 'w' ){
+    return 1;
 }
-if(buttonPress !='\0' && buttonPress == 0x20){ // If and only if the space bar is pressed, "YEEEEEEET!" is printed. This is going to be assigned as the shoot button
-    gotoxy(xPos,yPos);
-    printf("YEEEEEEET!");
-    gotoxy(xPos,yPos);
+if(buttonPress != '\0' && buttonPress == 'a'){
+    return 2;
 }
-there->xPosition = xPos;
-there->yPosition = yPos;
+if(buttonPress != '\0' && buttonPress == 's'){
+    return 3;
+}
+if(buttonPress != '\0' && buttonPress == 'd'){
+    return 4;
+}
+// Spacebar
+if(buttonPress !='\0' && buttonPress == 0x20){
+    return 5;
+}
+// Enter
+if(buttonPress !='\0' && buttonPress == 0x0D){
+    return 6;
+}
+ /*   switch (buttonPress){
+        case 'w':{
+            return 1;
+            break;
+        }
+        case 'a':{
+            return 2;
+            break;
+        }
+        case 's':{
+            return 3;
+            break;
+        }
+        case 'd':{
+            return 4;
+            break;
+        }
+        case 0x20:{
+            return 5;
+            break;
+        }
+        case 0x0D:{
+            return 6;
+            break;
+        }
+        default:
+            return '\0';
+    }*/
 uart_clear();
 }
 
+
+void Controller(int8_t moveDir, struct playerPos *there){
+uint16_t xPos = (*there).xPosition; // Updates the value of the x-coordinate
+uint8_t yPos = (*there).yPosition; // Updates the value of the y-coordinate
+    if (yPos > 0){ // Prevents the player from going off screen
+        if (moveDir == 1){
+            yPos--;
+        }
+    }
+    if (xPos > 0){ // Prevents the player from going off screen
+        if (moveDir == 2){
+            xPos--;
+        }
+    }
+    if (yPos < 66){ // Prevents the player from going off screen
+        if (moveDir == 3){
+            yPos++;
+        }
+    }
+    if (yPos < 271){ // Prevents the player from going off screen
+        if (moveDir == 4){
+            xPos++;
+            }
+    }
+    if (moveDir == 0){
+        return moveDir;
+    }
+    if (moveDir == '\0'){
+        return moveDir;
+    }
+there->xPosition = xPos;
+there->yPosition = yPos;
+}
