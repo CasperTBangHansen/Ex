d705e0cd115c2f48fb8888f11b3231fce25332c2
lampen <casperbanghansen@gmail.com>
2020-01-22 10:19:41 +0100

    With Ship skin changer

--- Eksamensprojekt/inc/DrawObjects.h
+++ Eksamensprojekt/inc/DrawObjects.h
@@ -8,7 +8,7 @@
 #include <stdlib.h>
 
 
-void drawMovingObjects(uint8_t enemySize, uint8_t moveDirection, struct drawItems *drawValues, struct player *player, struct enemy *enemy);
+void drawMovingObjects(uint8_t enemySize, uint8_t moveDirection, struct drawItems *drawValues, struct player *player, struct enemy *enemy, uint8_t ship);
 void drawEnemy(uint8_t control, uint16_t xPos, uint8_t yPos, uint16_t xPPos, uint8_t yPPos, struct drawItems *drawValues);
 
 #endif
